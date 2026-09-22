77a3d01d1caec100a19a03729a1e35989ab22b17
𝕀 ℕ 𝔽 𝔼 ℝ ℕ 𝕆 <101462689+infernogurala@users.noreply.github.com>
2026-09-22 21:24:11 +0530

    Implement distance capturing function for sensor
    Added a custom function to capture distance from the ultrasonic sensor.

--- ultra-sonic-sensor.c
+++ ultra-sonic-sensor.c
@@ -1,11 +1,36 @@
 // PIN CONFIG
 const int trigPin = 9;
 const int echoPin = 10;
-float duration, distance;
+float duration, distance, liveDistance;
 
+// SETUP PIN MODE
 void setup()
 {
   pinMode(triPin, OUTPUT);
   pinMode(echoPin, INPUT);
   Serial.begin(9600);
+}
+
+// <----CUSTOM FUNCTION---->
+float captureDistance()
+{
+  digitalWrite(trigPin, LOW):
+    delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH):
+    delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+    
+  duration = pulseIn(echoPin, HIGH);
+  distance = (duration*.0343)/2;
+  return distance;
+}
+
+// LOOP
+void loop()
+{
+  //get distance
+  liveDistance = captureDistance();
+  // print distance
+  Serial.print("Distance: ");
+  Serial.println(liveDistance);
 }
